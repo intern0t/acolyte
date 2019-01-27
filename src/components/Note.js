c2f005f61cc2adab0a5318e7fe56381c300fc563
Prashant Shrestha <intern0t@users.noreply.github.com>
2019-01-26 22:10:46 -0500

    Added Github flavored markdown stylesheet, highlight.js
    Signed-off-by: Prashant Shrestha <intern0t@users.noreply.github.com>

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,21 +1,16 @@
 import React, { Component } from "react";
 import { NotesConsumer } from "../contexts/NotesContext";
 import { ScrollSync, ScrollSyncPane } from "react-scroll-sync";
-import Remarkable from "remarkable";
-import RemarkableReactRenderer from "remarkable-react";
 import SyntaxHighlighter from "react-syntax-highlighter";
-import { github } from "react-syntax-highlighter/dist/styles/hljs";
-import CodeMirror from "react-codemirror";
-require("codemirror/mode/gfm/gfm");
+import { tomorrowNight } from "react-syntax-highlighter/dist/styles/hljs";
 
-const md = new Remarkable();
-md.renderer = new RemarkableReactRenderer({
-    components: {
-        pre: ({ content, params: language }) => (
-            <SyntaxHighlighter language={language}>{content}</SyntaxHighlighter>
-        )
-    }
-});
+// import brace from "brace";
+import AceEditor from "react-ace";
+import "brace/mode/markdown";
+import "brace/theme/github";
+import Markdown from "react-markdown-it";
+
+var hljs = require("highlight.js");
 
 export default class Note extends Component {
     state = {
@@ -47,33 +42,80 @@ export default class Note extends Component {
                     let subjectDetails = subjects.filter(
                         subject => subject.sid === activeSubjectID
                     );
-                    let padOptions = {
-                        lineNumbers: true,
-                        mode: "gfm",
-                        scrollBarStyle: null
-                    };
                     return (
                         <div className="note">
                             {subjectDetails && subjectDetails.length > 0
                                 ? subjectDetails[0].title
                                 : "Unknown"}
+
                             <ScrollSync>
                                 <div className="note-wrapper">
-                                    <ScrollSyncPane>
-                                        <div className="note-wrapper-raw">
-                                            <CodeMirror
-                                                value={this.state.note.content}
+                                    <ScrollSyncPane group="note">
+                                        <div
+                                            className="note-wrapper-raw"
+                                            style={{ overflow: "auto" }}
+                                        >
+                                            <AceEditor
                                                 onChange={this.onNoteChange}
-                                                options={padOptions}
+                                                value={this.state.note.content}
+                                                mode="markdown"
+                                                theme="github"
+                                                width={"100%"}
+                                                height={"100%"}
+                                                fontSize={14}
+                                                showPrintMargin={false}
+                                                editorProps={{
+                                                    $blockScrolling: true
+                                                }}
+                                                setOptions={{
+                                                    enableBasicAutocompletion: false,
+                                                    enableLiveAutocompletion: true,
+                                                    enableSnippets: false,
+                                                    animatedScroll: true,
+                                                    showLineNumbers: true,
+                                                    tabSize: 4
+                                                }}
                                             />
                                         </div>
                                     </ScrollSyncPane>
-                                    <ScrollSyncPane>
-                                        <div className="note-wrapper-rendered">
+                                    <ScrollSyncPane group="note">
+                                        <div
+                                            className="note-wrapper-rendered"
+                                            style={{ overflow: "auto" }}
+                                        >
                                             <div className="markdown-body">
-                                                {md.render(
-                                                    this.state.note.content
-                                                )}
+                                                <Markdown
+                                                    source={
+                                                        this.state.note.content
+                                                    }
+                                                    options={{
+                                                        langPrefix: "hljs-",
+                                                        typographer: true,
+                                                        quotes: "“”‘’",
+                                                        container:
+                                                            "markdown-body",
+                                                        highlight: function(
+                                                            str,
+                                                            lang
+                                                        ) {
+                                                            if (
+                                                                lang &&
+                                                                hljs.getLanguage(
+                                                                    lang
+                                                                )
+                                                            ) {
+                                                                try {
+                                                                    return hljs.highlight(
+                                                                        lang,
+                                                                        str
+                                                                    ).value;
+                                                                } catch (__) {}
+                                                            }
+
+                                                            return ""; // use external default escaping
+                                                        }
+                                                    }}
+                                                />
                                             </div>
                                         </div>
                                     </ScrollSyncPane>
@@ -83,6 +125,18 @@ export default class Note extends Component {
                     );
                 }}
             </NotesConsumer>
+        );
+    }
+}
+
+class CodeBlock extends React.PureComponent {
+    render() {
+        const { language, value } = this.props;
+
+        return (
+            <SyntaxHighlighter language={language} style={tomorrowNight}>
+                {value}
+            </SyntaxHighlighter>
         );
     }
 }
